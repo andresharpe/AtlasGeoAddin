050b7c97b65894e39aaf2e4b8868bfc75a1c2fed
andre <andre.sharpe@gmail.com>
2025-04-13 22:58:13 +0200

    Initial commit

---
+++ src/Atlas.Dna/GeoUdf.cs
@@ -0,0 +1,23 @@
+﻿namespace Atlas.Dna;
+
+using ExcelDna.Integration;
+
+public static partial class GeoUdf
+{
+    /// <summary>
+    /// Validates the latitude and longitude values.
+    /// </summary>
+    /// <param name="lat">The latitude value to validate.</param>
+    /// <param name="lon">The longitude value to validate.</param>
+    /// <returns>True if both latitude and longitude are valid; otherwise, false.</returns>
+    [ExcelFunction(Description = "Validates the latitude and longitude values.")]
+    public static bool GeoValidateLatLon(
+        [ExcelArgument(Name = "lat", Description = "The latitude value to validate.")] double lat,
+        [ExcelArgument(Name = "lon", Description = "The longitude value to validate.")] double lon)
+    {
+        return IsValidLatLon(lat, lon);
+    }
+
+    private static bool IsValidLatLon(double lat, double lon) =>
+        lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180;
+}
---
+++ src/Atlas.Dna/IntelliSenseAddin.cs
@@ -0,0 +1,17 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+
+namespace Atlas.Dna;
+
+public class IntelliSenseAddIn : IExcelAddIn
+{
+    public void AutoOpen()
+    {
+        IntelliSenseServer.Install();
+    }
+
+    public void AutoClose()
+    {
+        IntelliSenseServer.Uninstall();
+    }
+}
