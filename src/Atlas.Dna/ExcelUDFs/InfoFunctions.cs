e330e9c59944037977a28a65ee47fd893ac15f3b
andre <andre.sharpe@gmail.com>
2025-04-16 07:18:29 +0200

    Added version UDF

---
+++ src/Atlas.Dna/ExcelUDFs/InfoFunctions.cs
@@ -0,0 +1,15 @@
+﻿using ExcelDna.Integration;
+using System.Reflection;
+
+public static class InfoFunctions
+{
+    [ExcelFunction(Name = "GEO_VERSION", Description = "Returns the Atlas Geo add-in version.")]
+    public static string GetAddInVersion()
+    {
+        var version = Assembly.GetExecutingAssembly()
+                              .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                              ?.InformationalVersion ?? "unknown";
+
+        return version;
+    }
+}
